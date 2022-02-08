3788e490943d89798e57847892cdd56be022bb16
William <wilz.kelvin12@gmail.com>
2022-02-08 18:54:59 -0300

    Finishing the generic pages generation.

--- src/templates/Pages/index.tsx
+++ src/templates/Pages/index.tsx
@@ -2,18 +2,23 @@ import LinkWrapper from 'components/LinkWrapper'
 import * as S from './styles'
 import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline'
 
-const AboutTemplate = () => (
+export type PageTemplateProps = {
+  heading: string
+  body: string
+}
+
+const PageTemplate = ({ heading, body }: PageTemplateProps) => (
   <S.Content>
     <LinkWrapper href="/">
       <CloseOutline size={32} />
     </LinkWrapper>
 
-    <S.Heading>My Trips</S.Heading>
+    <S.Heading>{heading}</S.Heading>
 
     <S.Body>
-      <p>Testando</p>
+      <div dangerouslySetInnerHTML={{ __html: body }} />
     </S.Body>
   </S.Content>
 )
 
-export default AboutTemplate
+export default PageTemplate
