5283c5a0fb18b37fbb7ba3473d2a07fde118529e
William <wilz.kelvin12@gmail.com>
2022-02-11 20:08:21 -0300

    Added a mapbox style to the map and finished the places page generate.

---
+++ src/templates/Places/index.tsx
@@ -0,0 +1,62 @@
+import Image from 'next/image'
+
+import LinkWrapper from 'components/LinkWrapper'
+import { CloseOutline } from '@styled-icons/evaicons-outline'
+
+import * as S from './styles'
+import { useRouter } from 'next/router'
+
+type ImageProps = {
+  url: string
+  height: number
+  width: number
+}
+
+export type PlacesTemplateProps = {
+  place: {
+    slug: string
+    name: string
+    description?: {
+      html: string
+    }
+    gallery: ImageProps[]
+  }
+}
+
+export default function PlacesTemplate({ place }: PlacesTemplateProps) {
+  const router = useRouter()
+
+  if (router.isFallback) return null
+  return (
+    <>
+      <LinkWrapper href="/">
+        <CloseOutline size={32} aria-label="Go back to map" />
+      </LinkWrapper>
+
+      <S.Wrapper>
+        <S.Container>
+          <S.Heading>
+            <h1>{place.name}</h1>
+          </S.Heading>
+
+          <S.Body
+            dangerouslySetInnerHTML={{ __html: place.description?.html || '' }}
+          />
+
+          <S.Gallery>
+            {place.gallery.map((image, index) => (
+              <Image
+                src={image.url}
+                alt={place.name}
+                key={`photo-${index}`}
+                width={1000}
+                height={600}
+                quality={75}
+              />
+            ))}
+          </S.Gallery>
+        </S.Container>
+      </S.Wrapper>
+    </>
+  )
+}
